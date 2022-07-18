5a90c0cd7cd4519ae6ba2643f8bd13e5530d069f
Paras Mehta <parasmehta@parass-mbp.attlocal.net>
2022-07-18 12:56:08 -0400

    DO-588: implemented toDatabaseConfig method and set driver class

--- airbyte-integrations/connectors/source-netsuite-suiteanalytics/src/main/java/io/airbyte/integrations/source/netsuite_suiteanalytics/NetsuiteSuiteanalyticsSource.java
+++ airbyte-integrations/connectors/source-netsuite-suiteanalytics/src/main/java/io/airbyte/integrations/source/netsuite_suiteanalytics/NetsuiteSuiteanalyticsSource.java
@@ -19,26 +19,42 @@ public class NetsuiteSuiteanalyticsSource extends AbstractJdbcSource<JDBCType> i
 
   private static final Logger LOGGER = LoggerFactory.getLogger(NetsuiteSuiteanalyticsSource.class);
 
-  // TODO insert your driver name. Ex: "com.microsoft.sqlserver.jdbc.SQLServerDriver"
-  static final String DRIVER_CLASS = "driver_name_here";
+  static final String DRIVER_CLASS = "com.netsuite.jdbc.openaccess.OpenAccessDriver";
 
   public NetsuiteSuiteanalyticsSource() {
-    // TODO: if the JDBC driver does not support custom fetch size, use NoOpStreamingQueryConfig
+    // if the JDBC driver does not support custom fetch size, use NoOpStreamingQueryConfig
     // instead of AdaptiveStreamingQueryConfig.
     super(DRIVER_CLASS, AdaptiveStreamingQueryConfig::new, JdbcUtils.getDefaultSourceOperations());
   }
 
-  // TODO The config is based on spec.json, update according to your DB
+  // The config is based on spec.json, update according to your DB
   @Override
   public JsonNode toDatabaseConfig(final JsonNode config) {
-    // TODO create DB config. Ex: "Jsons.jsonNode(ImmutableMap.builder().put("username",
+    // create DB config. Ex: "Jsons.jsonNode(ImmutableMap.builder().put("username",
     // userName).put("password", pas)...build());
-    return null;
+    final StringBuilder jdbcUrl = new StringBuilder(String.format("jdbc:ns://%s:%s;ServerDataSource=%s;Encrypted=%s;NegotiateSSLClose=%s;CustomProperties=(AccountID=%s;RoleID=%s)",
+      config.get("host").asText(),
+      config.get("port").asText(),
+      config.get("server_data_source").asText(),
+      config.get("encrypted").asText(),
+      config.get("negotiate_ssl_close").asText(),
+      config.get("account_id").asText(),
+      config.get("role_id").asText()));
+
+    final ImmutableMap.Builder<Object, Object> configBuilder = ImmutableMap.builder()
+      .put("username", config.get("username").asText())
+      .put("jdbc_url", jdbcUrl.toString());
+
+    if (config.has("password")) {
+      configBuilder.put("password", config.get("password").asText());
+    }
+
+    return Jsons.jsonNode(configBuilder.build());
   }
 
   @Override
   public Set<String> getExcludedInternalNameSpaces() {
-    // TODO Add tables to exclude, Ex "INFORMATION_SCHEMA", "sys", "spt_fallback_db", etc
+    // Add tables to exclude, Ex "INFORMATION_SCHEMA", "sys", "spt_fallback_db", etc
     return Set.of("");
   }
 
